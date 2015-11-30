816f9a1fbc34bbc8160b25f5840da7dc0cf2e4a3
Erick Paepke <epaepke@gmail.com>
2015-11-29 18:43:15 -0800

    Complete required recursive functions

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,24 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
+/** Function that replicates document.getElementsByClassName(className).
+ *  @author E. D. Paepke
+ */
 
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className) {
+  /** Stores nodes of class "className". */
+  var elements = [];
+
+  /** Adds the input node to elements if it is of type "className".
+   *  Then recursively calls function on the children nodes. */
+  function addClassNode(node) {
+  	if (node.classList !== undefined) {
+	  	if (node.classList.contains(className)) {
+	  		elements.push(node);
+	  	}
+	}
+  	for (var i = 0; i < node.childNodes.length; i++) {
+  		addClassNode(node.childNodes[i]);
+  	}
+  }
+
+  addClassNode(document.body);
+  return elements;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,92 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
-
+/** Function that replicates JSON.stringify().
+ *  @author E. D. Paepke
+ */
+ 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	/** Establish array to store stringify entries. */
+    var content = [];
+
+    /** Executes stringify for primary value entries. */
+    if (typeof obj === 'number' || typeof obj === 'boolean') {
+        return obj.toString();
+    } else if (obj === null) {
+    	return 'null';
+    } else if (typeof obj === 'string') {
+    	return '"' + obj + '"';
+    }
+
+    objStringify(obj);
+
+    /** Runs through each key in object and determines how to stringify
+    /*  each entry. */
+    function objStringify(objArg) {
+    	var keys = [];
+    	if (objArg !== null && objArg !== undefined) {
+        	keys = Object.keys(objArg);
+        }
+        for (var i = 0; i < keys.length; i++) {
+           	if (objArg[keys[i]] === null) {
+           		content.push('"' + keys[i] + '":null')
+            } else if (Array.isArray(objArg[keys[i]])) {
+            	nestedObj(objArg, keys, i, ['[', ']']);
+            } else if (typeof objArg[keys[i]] === 'object') {
+            	nestedObj(objArg, keys, i, ['{','}'])
+            } else {
+            	addContent(objArg, keys, i);
+            }
+        }
+    }
+
+    /** Runs through each key in object and determines how to stringify
+    /*  each entry depending on type. */
+	function addContent(ob, keys, i) {
+		if (typeof ob[keys[i]] === 'string') {
+	        if (Array.isArray(ob)) {
+	    		content.push('"' + ob[keys[i]]+ '"');
+	    	} else {
+	    		content.push('"' + keys[i] + '":"' + ob[keys[i]]+ '"');
+	    	}
+	    }
+	    if (typeof ob[keys[i]] === 'number') {
+        	if (Array.isArray(ob)) {
+            	content.push(ob[keys[i]]);
+            } else {
+            	content.push('"' + keys[i] + '":' + ob[keys[i]]);
+            }
+        } 
+        if (typeof ob[keys[i]] === 'boolean') {
+	        if (Array.isArray(ob)) {
+	        	content.push(ob[keys[i]].toString());
+	        } else {
+	        	content.push('"' + keys[i] + '":' + ob[keys[i]].toString());
+	        }
+	    }
+	}
+
+    /** Runs through each key in the object and determines how to stringify
+    /*  each entry. */
+	function nestedObj(ob, keys, i, p) {
+    	if (Array.isArray(ob)) {
+            content.push(p[0] + '-join')
+            objStringify(ob[i]);
+            content.push('-join' + p[1]);
+        } else {
+            content.push('"' + keys[i] + '":' + p[0] + '-join');
+            objStringify(ob[keys[i]]);
+            content.push('-join' + p[1]);
+        }
+	}
+
+	/** Joins content in accordance with JSON format. */
+    content = content.join(',');
+    content = content.replace(/,-join/g,'');
+    content = content.replace(/-join,/g,'');
+    content = content.replace(/-join/g,'');
+
+    /** Adds outside container punctuation. */
+    if (Array.isArray(obj)) {
+    	return '[' + content + ']';
+    } else {
+    	return '{' + content + '}';
+    }
 };
